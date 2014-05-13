590b8ad9a6f31081426efecd12483a8a5164269d
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-13 16:27:30 -0700

    added secret_key_base to application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module HackerNews
   class Application < Rails::Application
+    config.secret_key_base = 'ce5a93fc37d1c99afd2ba4529879a9d14431478ad0be7f9e5f3567ea1f735003ac1aaeab5124d484d358e6e626b55d0a1e28f36cc5306164002b71945ca74c5d'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
