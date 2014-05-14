34d4f71eb555c244d460a5655db01600ec7f5b6c
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-13 18:44:37 -0700

    wrote a failing feature test for signing in

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe "Users", :js => false do
   describe "root page" do
+    before(:each){visit root_path}
     it "renders a sign up form" do
-      visit root_path
       expect(page).to have_content "Sign Up"
+    end
+    it "allows the a new user to sign up" do
+      fill_in "Name", :with => "Ned Stark"
+      fill_in "Password", :with => "winter"
+      click_on "Create User"
+      expect(page).to have_content "Ned Stark"
     end
   end
 end
