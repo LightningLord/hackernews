5b42dc66035bd24b2e8e5d0f72b1e709c1ed13e7
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-13 18:57:51 -0700

    unit test of user::create passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,13 @@ describe User do
   it {should have_many :posts}
   it {should have_many :comments}
 
+  context "create" do
+    it "sets user's name properly" do
+      params = {name: "Ned Stark", password: "winter"}
+      user = User.create(params)
+      user.save
+      expect(user.name).to eq "Ned Stark"
+    end
+
+  end
 end
