1927874fcb5323f1c205841f5aa0899db54b6179
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-13 18:45:06 -0700

    Set up User Signup
      *added bcrypt to gemfile
      *wrote user#create
      *wrote userscontroller#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user = User.create(params[:user])
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to root_path
+    end
+  end
 end
