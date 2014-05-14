9d55ac0264bbd96e255a847b5432c5120bb02e46
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-13 18:50:15 -0700

    user can sign up and see their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,14 @@ class UsersController < ApplicationController
   def create
     user = User.create(params[:user])
     if user.save
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       redirect_to root_path
     end
+  end
+
+  def show
+    @user = User.find(session[:user_id])
   end
 end
