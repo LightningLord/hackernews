e814e34f75a9cbb0ee7c76971ede5e725586151b
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-14 10:56:58 -0700

    wrote a failing feature test for login

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,6 +15,7 @@ describe "Users", :js => false do
   end
 
   describe "log in" do
+    let(:ned_stark){FactoryGirl.create(:user)}
     it "links to a login page" do
       visit root_path
       expect(page).to have_link "Log in"
@@ -23,7 +24,15 @@ describe "Users", :js => false do
     it "shows a login form" do
       visit new_session_path
       expect(page).to have_content "Name"
-      expect(page).to have_content "Password"
+      expect(page).to have_content "password"
+    end
+
+    it "allows a user to log in" do
+      visit new_session_path
+      fill_in "name", :with => ned_stark.name
+      fill_in "password", :with => ned_stark.password
+      click_button "Log in"
+      expect(page).to have_content "Log out"
     end
   end
 
