e60b3494527cdf8e1fedd7c9d10fec1f3df87ffd
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-14 08:27:28 -0700

    posts index shows titles and usernames of all posts

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -3,8 +3,8 @@ describe "posts", :js => false do
     let!(:new_post){FactoryGirl.create(:post)}
     it "renders a list of posts" do
       visit root_path
-      expect(page).to have_link :new_post.title
-      expect(page).to have_content :new_post.user.name
+      expect(page).to have_link new_post.title
+      expect(page).to have_content new_post.user.name
     end
   end
 end
