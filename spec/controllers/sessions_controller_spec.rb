64160260167a1612cfdfa393773c6bb89d02645a
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-14 11:12:35 -0700

    controller test for invalid login passing

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,5 @@
 describe SessionsController do
+  let(:ned_stark) {FactoryGirl.create(:user)}
   context "#new" do
     it "is successful" do
       get :new
@@ -6,14 +7,20 @@ describe SessionsController do
     end
   end
 
-  context "#create" do
-    let(:ned_stark) {FactoryGirl.create(:user)}
+  context "#create with valid user" do
     before(:each) { post :create, name: ned_stark.name, password: ned_stark.password }
     it "sets session[:user_id] for a valid user" do
       expect(session[:user_id]).to eq ned_stark.id
     end
     it "redirects to root_path" do
       expect(response).to redirect_to root_path
+    end
+  end
+
+  context "#create with invalid user" do
+    it "does not set a session for an invalid user" do
+      post :create, name: ned_stark.name, password: "winter"
+      expect(session[:user_id]).to be_nil
     end
   end
 end
