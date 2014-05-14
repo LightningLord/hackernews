227d329120b7c0dacd7544cafd977b069e92fed1
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-14 09:25:47 -0700

    wrote feature and controller tests for posts#show

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,4 +10,17 @@ describe PostsController do
       expect(response).to be_successful
     end
   end
+
+  context '#show' do
+    let(:new_post) {FactoryGirl.create(:post)}
+    before(:each) {get :show, :id=> new_post.id}
+
+    it "is successful" do
+      expect(response).to be_successful
+    end
+
+    it "assigns @post to the correct post" do
+      expect(assigns(:post)).to eq new_post
+    end
+  end
 end
