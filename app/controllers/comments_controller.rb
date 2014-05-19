ef439339a0a43cb3c50837be1f67fda6a9d9de6c
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-19 10:03:58 -0700

    implemented comments#update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,29 @@
 class CommentsController < ApplicationController
   include ApplicationHelper
   def create
-    comment = Post.find(params[:post_id]).comments.build(params[:comment])
-    comment.user = current_user
-    comment.save
-    redirect_to post_path(comment.post)
+    if current_user
+      comment = Post.find(params[:post_id]).comments.build(params[:comment])
+      comment.user = current_user
+      comment.save
+      redirect_to post_path(comment.post)
+    else
+      redirect_to new_session_path
+    end
+
   end
 
   def edit
+    redirect_to new_session_path unless current_user
     @comment = Comment.find(params[:id])
   end
+
+  def update
+    comment = Comment.find(params[:id])
+    if comment.update_attributes(params[:comment])
+      redirect_to post_path(comment.post)
+    else
+      redirect_to edit_post_comment_path(comment.post, comment)
+    end
+  end
+
 end
