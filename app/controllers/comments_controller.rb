cf866893d2e44e90262beaa4880982d7992bd65f
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-19 10:28:28 -0700

    Implemented comments#destroy
    *also refactored comments#update and posts#update to check for owner

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,11 +19,17 @@ class CommentsController < ApplicationController
 
   def update
     comment = Comment.find(params[:id])
-    if comment.update_attributes(params[:comment])
+    if comment.user == current_user && comment.update_attributes(params[:comment])
       redirect_to post_path(comment.post)
     else
       redirect_to edit_post_comment_path(comment.post, comment)
     end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy if comment.user == current_user
+    redirect_to post_path(comment.post)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,8 +35,7 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-    redirect_to root_path unless current_user == post.user
-    if post.update_attributes(params[:post])
+    if post.user == current_user && post.update_attributes(params[:post])
       redirect_to post_path(post)
     else
       redirect_to edit_post_path(post)
