57a0144f3648c99b6e33ffa363143ba6770fcf4d
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-19 08:26:33 -0700

    wrote failing feature test for editing a post

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -4,27 +4,35 @@ describe "comments", :js => false do
     let(:ned_stark){FactoryGirl.create(:user)}
     let(:tyrion){FactoryGirl.create(:user)}
     let(:new_post){FactoryGirl.create(:post, :user => ned_stark)}
-    let!(:new_comment){FactoryGirl.create(:comment, :user=> tyrion, :post=> new_post)}
     context "any user" do
+      let!(:new_comment){FactoryGirl.create(:comment, :user=> tyrion, :post=> new_post)}
       it "can see all of the comments for a post" do
         visit post_path(new_post)
         expect(page).to have_content(new_comment.content)
       end
     end
     context "logged in user" do
+      let!(:ned_comment){FactoryGirl.create(:comment,
+        :user => ned_stark, :post => new_post)}
       before(:each) do
         visit new_session_path
         fill_in "name", :with => ned_stark.name
         fill_in "password", :with => ned_stark.password
         click_button "Log in"
+        visit post_path(new_post)
       end
       it "can comment on a post" do
-        visit post_path(new_post)
         fill_in "comment[content]", :with => "Hear me roar"
         click_button "Create Comment"
         expect(page).to have_content "Hear me roar"
       end
-      it "can edit own comment"
+      it "can edit own comment" do
+        click_link "Edit Comment"
+        fill_in "comment[content]", :with => "Winter is coming"
+        click_button "Update Comment"
+        expect(page).to have_content "Winter is coming"
+      end
+
       it "can delete own comment"
     end
   end
