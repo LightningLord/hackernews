d055051fea7ebe7922996502da295dc347c14b8c
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-19 16:52:47 -0700

    refactored comments#update and comments#edit actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,14 +9,15 @@ class CommentsController < ApplicationController
 
   def edit
     @comment = Comment.find(params[:id])
+    render :partial => "edit", locals: {:comment => @comment, :post => @comment.post}
   end
 
   def update
-    comment = Comment.find(params[:id])
-    if comment.user == current_user && comment.update_attributes(params[:comment])
-      redirect_to post_path(comment.post)
+    @comment = Comment.find(params[:id])
+    if @comment.user == current_user && @comment.update_attributes(params[:comment])
+      render :partial => "comment", locals: {:comment => @comment, :post => @comment.post}
     else
-      redirect_to edit_post_comment_path(comment.post, comment)
+      render :partial => "not_owner"
     end
   end
 
