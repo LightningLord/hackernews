fa03457b06093eff7fa32ca674b915ba74c2d08f
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-18 20:04:23 -0700

    post show page displays all comments

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,9 +1,10 @@
+require 'spec_helper'
 describe "comments", :js => false do
   describe "on the post show page" do
     let(:ned_stark){FactoryGirl.create(:user)}
     let(:tyrion){FactoryGirl.create(:user)}
     let(:new_post){FactoryGirl.create(:post, :user => ned_stark)}
-    let(:new_comment){FactoryGirl.create(:comment, :user=> tyrion, :post=> new_post)}
+    let!(:new_comment){FactoryGirl.create(:comment, :user=> tyrion, :post=> new_post)}
     context "any user" do
       it "can see all of the comments for a post" do
         visit post_path(new_post)
