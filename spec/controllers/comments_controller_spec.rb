25563d232b9669f2778a1e5fee5ff7b63411a02b
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-19 08:31:19 -0700

    wrote failing controller test for comments#edit

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 describe CommentsController do
-  context '#create' do
     let(:my_post){FactoryGirl.create(:post)}
+  context '#create' do
     it "creates a comment" do
       expect { post :create, :post_id => my_post.id, :comment =>
         FactoryGirl.attributes_for(:comment) }.to change {Comment.count}.by(1)
@@ -13,4 +13,15 @@ describe CommentsController do
       expect(response).to be_redirect
     end
   end
+
+  context '#edit' do
+    let(:my_comment){FactoryGirl.create(:comment, :post => my_post)}
+    it "is successful" do
+      get :edit, :post_id => my_post.id, :id => my_comment.id
+      expect(response).to be_successful
+    end
+
+    it "edits a comment"
+  end
+
 end
