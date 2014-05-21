f0897ae8a8300c98f2ca60370cc69722e4c6bfd5
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-21 15:08:07 -0700

    feature tests for votes

---
+++ spec/features/votes_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+describe "Votes", :js => true do
+  let(:ned_stark){FactoryGirl.create(:user)}
+  let(:new_post){FactoryGirl.create(:post)}
+  let!(:new_comment){FactoryGirl.create(:comment, :post=> new_post)}
+  before(:each) do
+    visit new_session_path
+    fill_in "name", :with => ned_stark.name
+    fill_in "password", :with => ned_stark.password
+    click_button "Log in"
+    visit post_path(new_post)
+  end
+
+  describe "vote on a question" do
+    it "can upvote a question" do
+      click_on "Upvote this question"
+      wait_for_ajax_to_finish
+      expect(page).to have_content new_post.reload.vote_count
+    end
+    it "can downvote a question"
+  end
+
+  describe "vote on a comment" do
+    it "can upvote a comment"
+    it "can downvote a comment"
+  end
+end
