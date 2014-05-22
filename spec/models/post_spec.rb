8008cbab4948fc8c79311f5597133f5822d838c6
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-22 13:27:06 -0700

    wrote a unit test for update_vote_count

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,4 +9,11 @@ describe Post do
   it {should validate_presence_of :user}
 
   it {should have_many :votes}
+
+  context "updateVoteCount" do
+    let(:my_post){FactoryGirl.create(:post)}
+    it "should change the vote properly" do
+      expect{my_post.update_vote_count(1)}.to change{my_post.reload.vote_count}.by(1)
+    end
+  end
 end
