9f7b9050ffe3396db4c4a588dbce28cf6a793071
Jason Lo <mr.lo.mhs@gmail.com>
2014-05-22 13:13:12 -0700

    wrote javascript to handle asynchronous comment votes

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,9 +1,13 @@
 $(function(){
-  $('.post_vote').on("ajax:success", postVoteHandler.updatePoints)
+  $('.post_vote').on("ajax:success", voteHandler.updatePostPoints)
+  $('.comment_vote').on("ajax:success", voteHandler.updateCommentPoints)
 })
 
-var postVoteHandler = {
-  updatePoints: function(e, data, status, xhr){
+var voteHandler = {
+  updatePostPoints: function(e, data, status, xhr){
     $('#post_points').html(xhr.responseJSON.points)
+  },
+  updateCommentPoints: function(e, data, status, xhr){
+    $(e.target).parent().parent().find(".comment_points").html(xhr.responseJSON.points)
   }
 }
